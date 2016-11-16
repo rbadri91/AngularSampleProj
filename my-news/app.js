b94b2bb27e7a700f59cfee465010156c9c6a8c74
badri <rbadri01@gmail.com>
2016-11-16 04:02:27 -0500

    Partially funtioning app

--- my-news/app.js
+++ my-news/app.js
@@ -5,8 +5,13 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+require('./models/Posts');
+require('./models/Comments');
 var index = require('./routes/index');
 var users = require('./routes/users');
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/news');
 
 var app = express();
 
---
+++ my-news/public/javascripts/myNewsApp.js
@@ -0,0 +1,73 @@
+var app = angular.module("myNews",['ui.router']);
+app.factory('posts',[function(){
+	var o ={
+		posts:[
+		 {title: 'post 1', upvotes: 4 ,comments: {}},
+  {title: 'post 2', upvotes: 6 , comments: {}},
+  {title: 'post 3', upvotes: 9, comments: {}},
+  {title: 'post 4', upvotes: 7 , comments: {}},
+  {title: 'post 5', upvotes: 5 ,comments: {}}
+  ]
+	};
+	return o;
+}]);
+app.controller('mainController',['$scope','posts',function($scope,posts){
+	$scope.posts = posts.posts;
+	$scope.addPost =function(){
+		if(!$scope.title || $scope.title === '') { return; }
+		$scope.posts.push({title:$scope.title,
+			link:$scope.link,
+			upvotes:0,
+			comments: [
+    				{author: 'Joe', body: 'Cool post!', upvotes: 0},
+    				{author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+  			]
+		});
+		$scope.title = '';
+		$scope.link = '';
+	};
+	$scope.incrementUpvotes=function(post){
+		post.upvotes +=1;
+	}
+}]);
+app.controller('PostsCtrl', [
+'$scope',
+'$stateParams',
+'posts',
+function($scope, $stateParams, posts){
+	$scope.post = posts.posts[$stateParams.id];
+	$scope.addComment = function(){
+  			if($scope.body === '') { return; }
+  			if(!$scope.post.comments){$scope.post.comments={};}
+ 				 $scope.post.comments.push({
+    			body: $scope.body,
+    			author: 'user',
+    			upvotes: 0
+  				});
+  $scope.body = '';
+  $scope.incrementUpvotes=function(comment){
+		comment.upvotes +=1;
+  }
+  $scope.modifyOrderFunction = function() {
+    if ($scope.post.comments) {
+    	console.log("it comes inside");
+        return '-upvotes';
+    }
+    else {
+        return;
+    }
+}
+};
+}]);
+app.config(['$stateProvider','$urlRouterProvider',function($stateProvider,$urlRouterProvider){
+$stateProvider.state('home',{
+	url :'/home',
+	templateUrl :'/home.html',
+	controller: 'mainController'
+}).state('posts', {
+  url: '/posts/{id}',
+  templateUrl: '/posts.html',
+  controller: 'PostsCtrl'
+});
+$urlRouterProvider.otherwise('home');
+}]);
